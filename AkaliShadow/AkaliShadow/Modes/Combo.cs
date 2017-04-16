e7617d2f3baa12c21a7d51a75f22b2bc8d16de21
xTeKillax <colty_ak@hotmail.fr>
2017-04-16 15:36:42 +0200

    Revert "Jump if Q in air during combo"
    This reverts commit 088f6c9ab0987a1e1f30eebd5d2e3be115055e23.

--- AkaliShadow/AkaliShadow/Modes/Combo.cs
+++ AkaliShadow/AkaliShadow/Modes/Combo.cs
@@ -31,7 +31,7 @@ namespace AkaliShadow.Modes
             Combat.CastItems(target);
 
             //If marked we will do everything to proc it
-            if (Combat.HasBuff(target, "AkaliMota") || Program.QInAir)
+            if (Combat.HasBuff(target, "AkaliMota"))
             {
                 if(Player.Instance.Distance(target) <= Player.Instance.GetAutoAttackRange())
                 {
